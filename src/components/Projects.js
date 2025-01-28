934ccb5fc9acede53b0860ec2a30e54a25d1a576
Noah Greenbaum <ngreenbaum444@gmail.com>
2025-01-28 11:10:55 -0500

    Rearranged order of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,23 @@ const Projects = () => {
         <section id="projects">
             <h2>My Projects</h2>
             <div className="project-list">
+            <div className="project-card">
+                    <h3>Duffy Paint Website</h3>
+                    <img src={DuffyPaintImage} alt="Duffy Paint" />
+                    <p>
+                        A website for Duffy Paint featuring a front-end for customers to explore services and a back-end administrative page for the owner to manage content.
+                    </p>
+                    <a href="https://duffypaint.com" target="_blank" rel="noreferrer" className="projectLink">Visit Website</a>
+                </div>
+                <div className="project-card">
+                    <h3>Urban Head Islands in Ohio</h3>
+                    <p>
+                        This project was a final project for a remote sensing class. The project was to identify urban heat islands in Ohio using remote sensing data. The project
+                        used Landsat 8 data to identify the urban heat islands using thresholding, kmeans clustering, and random forest. The project was done in a group of 3 people.
+                        Technologies used we EarthExplorer, Python3, and QGIS. 
+                    </p>
+                    <button onClick={() => setShowPopup(true)}>View Presentation and Paper</button>
+                </div>
                 <div className="project-card">
                     <h3>Food Expiration Tracker</h3>
                     <p>
@@ -29,14 +46,6 @@ const Projects = () => {
                     <a href="https://github.com/Noahg444/2016-Summer-Olympics-Explorer-MAUI-.NET" target="_blank" rel="noreferrer" className="projectLink">View on GitHub</a>
                 </div>
                 <div className="project-card">
-                    <h3>Duffy Paint Website</h3>
-                    <img src={DuffyPaintImage} alt="Duffy Paint" />
-                    <p>
-                        A website for Duffy Paint featuring a front-end for customers to explore services and a back-end administrative page for the owner to manage content.
-                    </p>
-                    <a href="https://duffypaint.com" target="_blank" rel="noreferrer" className="projectLink">Visit Website</a>
-                </div>
-                <div className="project-card">
                     <h3>MineSweeper</h3>
                     <p>
                         This is an implementation of MineSweeper game using MVC architecture in Python. The game can be played in the console and has a simple interface or in
@@ -44,15 +53,6 @@ const Projects = () => {
                         has a test board loading option allowing the user to test knowing the game board layout. The technologies used in the project were Python3.
                     </p>
                     <a href="https://github.com/Noahg444/MineSweeper" target="_blank" rel="noreferrer" className="projectLink">View on GitHub</a>
-                </div>
-                <div className="project-card">
-                    <h3>Urban Head Islands in Ohio</h3>
-                    <p>
-                        This project was a final project for a remote sensing class. The project was to identify urban heat islands in Ohio using remote sensing data. The project
-                        used Landsat 8 data to identify the urban heat islands using thresholding, kmeans clustering, and random forest. The project was done in a group of 3 people.
-                        Technologies used we EarthExplorer, Python3, and QGIS. 
-                    </p>
-                    <button onClick={() => setShowPopup(true)}>View Presentation and Paper</button>
                 </div>
             </div>
             {showPopup && (
